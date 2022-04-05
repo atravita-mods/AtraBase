793565f64b97495f26382dc164814b885bdde3bb
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-04-04 21:04:40 -0400

    add functions to get empty containers

---
+++ AtraBase/Collections/EmptyContainers.cs
@@ -0,0 +1,25 @@
+﻿namespace AtraBase.Collections;
+
+/// <summary>
+/// Gets empty containers.
+/// </summary>
+internal static class EmptyContainers
+{
+    /// <summary>
+    /// Gets a blank dictionary of the following type.
+    /// </summary>
+    /// <typeparam name="TKey">The key type.</typeparam>
+    /// <typeparam name="TValue">The value type.</typeparam>
+    /// <returns>An empty dictionary.</returns>
+    internal static Dictionary<TKey, TValue> GetEmptyDictionary<TKey, TValue>()
+        where TKey : notnull
+        => new();
+
+    /// <summary>
+    /// Gets a blank list of the following type.
+    /// </summary>
+    /// <typeparam name="TKey">They key type.</typeparam>
+    /// <returns>An empty list.</returns>
+    internal static List<TKey> GetEmptyList<TKey>()
+        => new();
+}
