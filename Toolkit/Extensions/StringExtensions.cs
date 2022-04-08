3dfb86ee4f600081eaf9d35ac821e6630a06ec2a
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-04-07 23:40:12 -0400

    Fast reflection

--- Toolkit/Extensions/StringExtensions.cs
+++ Toolkit/Extensions/StringExtensions.cs
@@ -1,7 +1,18 @@
 ﻿namespace AtraBase.Toolkit.Extensions;
 
+/// <summary>
+/// Extension methods on strings.
+/// </summary>
 internal static class StringExtensions
 {
+    /// <summary>
+    /// Gets the Nth occurance of a specific unicode char in a string.
+    /// </summary>
+    /// <param name="str">String to search in.</param>
+    /// <param name="item">Char to search for.</param>
+    /// <param name="count">N.</param>
+    /// <returns>Index of the char, or -1 if not found.</returns>
+    [Pure]
     internal static int NthOccuranceOf(this string str, char item, int count = 1)
     {
         for (int i = 0; i < str.Length; i++)
@@ -14,6 +25,14 @@ internal static class StringExtensions
         return -1;
     }
 
+    /// <summary>
+    /// Gets the Nth occurance from the end of a specific unicode char in a string.
+    /// </summary>
+    /// <param name="str">String to search in.</param>
+    /// <param name="item">Char to search for.</param>
+    /// <param name="count">N.</param>
+    /// <returns>Index of the char, or -1 if not found.</returns>
+    [Pure]
     internal static int NthOccuranceFromEnd(this string str, char item, int count = 1)
     {
         for (int i = str.Length - 1; i >= 0; i--)
