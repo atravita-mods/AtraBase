8ca08721bb27884a62920feb0637dd969f2f551d
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-07-12 15:51:30 -0400

    bit of cleanup

---
+++ AtraBase/Toolkit/GCHelperFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime;
+
+namespace AtraBase.Toolkit;
+
+/// <summary>
+/// Helper functions for GC.
+/// </summary>
+internal static class GCHelperFunctions
+{
+    /// <summary>
+    /// Asks for a full, compacting GC.
+    /// </summary>
+    internal static void RequestFullGC()
+    {
+        GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+        GC.Collect();
+    }
+}
