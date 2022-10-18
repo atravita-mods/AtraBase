86a2c11f60920a1af92c34fb97cfa1625410ed1c
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2022-10-18 13:05:27 -0400

    prewarm randoms

---
+++ AtraBase/Toolkit/Extensions/RandomExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace AtraBase.Toolkit.Extensions;
+
+/// <summary>
+/// Extensions for Random.
+/// </summary>
+public static class RandomExtensions
+{
+    // just a buffer to stash prewarm values in.
+    private static byte[] buffer = new byte[8];
+
+    /// <summary>
+    /// Prewarms a random.
+    /// </summary>
+    /// <param name="random">The random to prewarm.</param>
+    public static void PreWarm(this Random random)
+    {
+        int prewarm = random.Next(64);
+        for (int i = 0; i < prewarm; i++)
+        {
+            random.NextBytes(buffer);
+        }
+
+        prewarm = random.Next(64);
+        for (int i = 0; i < prewarm; i++)
+        {
+            random.NextBytes(buffer);
+        }
+    }
+}
