d3ac10efa08dad6368ae0375763e9cf67b0d6a1a
atravita-mods <94934860+atravita-mods@users.noreply.github.com>
2024-02-25 20:42:52 -0600

    I haven't a clue anymore.

--- AtraBase/Toolkit/Shims/NetSeven/UnreachableException.cs
+++ AtraBase/Toolkit/Shims/NetSeven/UnreachableException.cs
@@ -2,6 +2,9 @@
 
 namespace System.Diagnostics;
 
+/// <summary>
+/// An exception that should never be thrown.
+/// </summary>
 public sealed class UnreachableException : Exception
 {
     /// <summary>
@@ -11,11 +14,20 @@ public sealed class UnreachableException : Exception
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UnreachableException"/> class.
+    /// </summary>
+    /// <param name="message">The message that should be attached.</param>
     public UnreachableException(string? message)
         : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UnreachableException"/> class.
+    /// </summary>
+    /// <param name="message">The message that should be attached.</param>
+    /// <param name="innerException">The inner exception.</param>
     public UnreachableException(string? message, Exception? innerException)
         : base(message, innerException)
     {
